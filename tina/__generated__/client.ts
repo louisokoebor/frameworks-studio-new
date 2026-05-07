580f376fce392a75ef054a07c9b83ec938826f07
louisokoebor <louisokoebor@gmail.com>
2026-05-07 13:00:41 +0100

    styling edit for pricing cards

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'ed9f72bf5d2f71b060b0b94606f39f67a81423dd', queries,  });
 export default client;
   
